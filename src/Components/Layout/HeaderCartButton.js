e945312d4c41af616f1729c76086a2685120ed54
Sachin Jakhar <sachinjakhar302@gmail.com>
2023-05-09 15:51:07 +0530

    final-web-page-with-added-animation

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,25 +1,45 @@
-import React,{useContext} from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
+import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../Store/cart-context";
 
-const HeaderCartButton=(props)=>{
-    const cartCtx=useContext(CartContext);
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-    const numberOfCartItems=cartCtx.items.reduce((currentNum,item)=>{
-        return currentNum+item.amount
-    },0)
+  const [animateButton, setButtonAnimate] = useState(false);
 
-    return(
-        <button className={classes.button} onClick={props.onConfirm}>
-            <span className={classes.icon} >
-                <CartIcon/>
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>{numberOfCartItems}</span>
-        </button>
-    );
+  const numberOfCartItems = items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${animateButton ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonAnimate(true);
+
+    const timer = setTimeout(() => {
+      setButtonAnimate(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onConfirm}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;

a73c5236425e1eaf835b54b16e94c4c1df776bb3
Sachin Jakhar <sachinjakhar302@gmail.com>
2023-05-14 15:15:39 +0530

    fetching-data-with-http-request

--- src/Components/Meals/AvilableMeals.js
+++ src/Components/Meals/AvilableMeals.js
@@ -1,52 +1,77 @@
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
 
 import MealsItem from "./MealsItem/MealsItem";
 import classes from "./AvilableMeals.module.css";
 import Card from "../UI/Card";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvilableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
-    return (
-      <MealsItem
-        id={meal.id}
-        key={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />
-    );
-  });
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, seterror] = useState(null);
+
+  const fetchMealsHandler = useCallback(() => {
+    const fetchMealsFunction = async () => {
+      setIsLoading(true);
+      seterror(null);
+      try {
+        const response = await fetch(
+          "https://react-food-app-backend-default-rtdb.firebaseio.com/Meals.json"
+        );
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const data = await response.json();
+
+        const fetchedMeals = [];
+        for (const key in data) {
+          fetchedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+
+        setMeals(fetchedMeals);
+      } catch (error) {
+        seterror(error.message);
+      }
+      setIsLoading(false);
+    };
+    fetchMealsFunction();
+  }, []);
+
+  useEffect(() => {
+    fetchMealsHandler();
+    console.log("run");
+  }, [fetchMealsHandler]);
+
+  let content = <p>No meals found!</p>;
+  if (meals.length > 0) {
+    content = meals.map((meal) => {
+      return (
+        <MealsItem
+          id={meal.id}
+          key={meal.id}
+          name={meal.name}
+          description={meal.description}
+          price={meal.price}
+        />
+      );
+    });
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        <ul>{content}</ul>
       </Card>
     </section>
   );
